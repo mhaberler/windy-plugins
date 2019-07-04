346e42ff4d4ae806881d130227e093570a2d2177
rittels <44720431+rittels@users.noreply.github.com>
2019-07-04 07:50:40 +0200

    Delete config.js

--- _plugins/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	displayName: 'traj',
-	hook: 'contextmenu',
-    className: 'plugin-lhpane plugin-mobile-fullscreen',
-	exclusive: 'lhpane',
-    dependencies: [
-		'https://windy.com/js/colorpicker.js'
-	],
-}
